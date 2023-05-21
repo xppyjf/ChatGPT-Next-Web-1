4b780f2acc2c399d80743ecd0b0cfc80ffb2efad
xppyjf <76797941+xppyjf@users.noreply.github.com>
2023-05-22 04:08:37 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const OWNER = "aibxw";
+export const REPO = "AIbxw";
+export const REPO_URL = `https://www.aibxw.com`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const UPDATE_URL = `${REPO_URL}#up`;
+export const FETCH_COMMIT_URL = `https://www.aibxw.com`;
+export const FETCH_TAG_URL = `https://www.aibxw.com`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {

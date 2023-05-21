b8096be1db8406c2e4eb24418814fc5407869a17
xppyjf <76797941+xppyjf@users.noreply.github.com>
2023-05-22 04:25:12 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -323,9 +323,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>Ai三杯茶</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              https://chat.aibxw.com
             </div>
             <div className={styles["icons"]}>
               <Avatar avatar={config.avatar}></Avatar>
